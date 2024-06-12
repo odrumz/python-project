f7dab7b368781f5375fc0b3866dc80baddb4d5e5
DerrickOngoma <Odrumz64@gmail.com>
2024-06-12 13:29:43 +0300

    progression

--- lib/cli.py
+++ lib/cli.py
@@ -1,114 +1,3 @@
-# import sqlite3
-# from helpers import exit_program
-
-# def create_tables():
-#     conn = sqlite3.connect("farm.db")
-#     cursor = conn.cursor()
-
-#     cursor.execute('''CREATE TABLE IF NOT EXISTS products (
-#         farm_id INTEGER PRIMARY KEY AUTOINCREMENT,
-#         farm_product TEXT NOT NULL,
-#         quantity INTEGER,
-#         day_id INTEGER,
-#         FOREIGN KEY (day_id) REFERENCES days(day_id)
-#     )''')
-
-#     cursor.execute('''CREATE TABLE IF NOT EXISTS days (
-#         day_id INTEGER PRIMARY KEY AUTOINCREMENT,
-#         day TEXT NOT NULL
-#     )''')
-
-#     conn.commit()
-#     conn.close()
-
-# create_tables()
-
-# class Farm:
-#     def __init__(self) -> None:
-#         self.conn = sqlite3.connect("farm.db")
-#         self.cursor = self.conn.cursor()
-
-#     def add_product(self, farm_product, quantity):
-#         self.cursor.execute('INSERT INTO products (farm_product, quantity) VALUES (?, ?)',
-#                             (farm_product, quantity))
-#         self.conn.commit()
-
-#     def add_day(self, day):
-#         self.cursor.execute('INSERT INTO days (day) VALUES (?)', (day,))
-#         self.conn.commit()
-
-#     def record(self, day_id, farm_id):
-#         self.cursor.execute('SELECT * FROM products WHERE day_id = ? AND farm_id = ?', (day_id, farm_id))
-#         return self.cursor.fetchone() is not None
-
-#     def link(self, day_id, farm_id):
-#         if self.record(day_id):
-#             self.cursor.execute('UPDATE products SET day_id = ? WHERE farm_id = ?',
-#                                 (day_id, farm_id))
-#             self.conn.commit()
-#             print(f"Day id ({day_id}) is linked to farm_id ({farm_id}).")
-#         else:
-#             print(f'The details do not exist')
-
-#     def details(self):
-#         query = '''SELECT products.farm_id, products.farm_product, products.quantity, days.day_id, days.day
-#                 FROM products
-#                 LEFT JOIN days ON products.day_id = days.day_id'''
-#         self.cursor.execute(query)
-#         return self.cursor.fetchall()
-
-        
-#     def delete_product(self, farm_id):
-#         if self.record(farm_id):  # Pass the farm_id argument to the record method
-#             self.cursor.execute('DELETE FROM products WHERE farm_id = ?', (farm_id,))
-#             self.conn.commit()
-#             print(f"Product with farm_id ({farm_id}) has been deleted.")
-#         else:
-#             print(f'No product found with farm_id ({farm_id}).')
-
-
-# def menu():
-#     print("Please select an option:")
-#     print("1. Add product")
-#     print("2. Add day")
-#     print("3. Link the product with the day collected")
-#     print("4. Display details")
-#     print("5. Search product details")
-#     print("6. Delete product details")
-#     print("0. Exit the program")
-
-# def main():
-#     db = Farm()
-#     while True:
-#         menu()
-#         choice = input(">>> ")
-#         if choice == "0":
-#             exit_program()
-#         elif choice == "1":
-#             farm_product = input("Enter product: ")
-#             quantity = input("Enter quantity: ")
-#             db.add_product(farm_product, quantity)
-#         elif choice == "2":
-#             day = input("Enter day: ")
-#             db.add_day(day)
-#         elif choice == "3":
-#             day_id = int(input("Enter day id: "))
-#             farm_id = int(input("Enter farm id: "))
-#             db.link(day_id, farm_id)
-#         elif choice == "4":
-#             details = db.details()
-#             for detail in details:
-#                 print(f"On {detail[4]}, {detail[2]} {detail[1]} were collected.")
-#         # elif choice == "5":
-#         elif choice == "6":
-#             farm_id = int(input("Enter farm_id to DELETE : "))
-#             db.delete_product(farm_id)
-#         else:
-#             print("Invalid option")
-
-# if __name__ == "__main__":
-#     main()
-
 
 # Import libraries
 import sqlite3
@@ -175,6 +64,9 @@ class Farm:
                    LEFT JOIN days ON products.day_id = days.day_id'''
         self.cursor.execute(query)
         return self.cursor.fetchall()
+    
+    def search_detail(self,farm_id):
+        pass
 
     def delete_product(self, farm_id):
         # Check if farm_id exists
@@ -195,7 +87,8 @@ def menu():
     print("2. Add day")
     print("3. Link the product with the day collected")
     print("4. Display details")
-    print("5. Delete product")
+    print("5. Search detail")
+    print("6. Delete product")
     print("0. Exit the program")
 
 # Main function
@@ -221,7 +114,7 @@ def main():
             details = db.details()
             for detail in details:
                 print(f"On {detail[4]}, {detail[2]} {detail[1]} were collected.")
-        elif choice == "5":
+        elif choice == "6":
             farm_id = int(input("Enter farm_id to DELETE : "))
             db.delete_product(farm_id)
         else:
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,15 +1,11 @@
 # lib/helpers.py
 
-# from cli import Farm
 
-# db = Farm()
 def helper_1():
     pass
-#     farm_product = input("Enter product")
-#     quantity = input("Enter quantity")
-#     db.add_product(farm_product,quantity)
 
 
 def exit_program():
     print("Goodbye!")
     exit()
+    
